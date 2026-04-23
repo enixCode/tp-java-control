58ffb1e60e3429e3ee88c664ef0f99e78abfbb9a
Noan Madiot <noanmadiot32@gmail.com>
2026-04-23 15:03:40 +0200

    commit initialisation tp

---
+++ src/main/java/tpjava/tpjavacontrole/TpJavaControleApplication.java
@@ -0,0 +1,13 @@
+package tpjava.tpjavacontrole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpJavaControleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpJavaControleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tpjava/tpjavacontrole/TpJavaControleApplicationTests.java
@@ -0,0 +1,13 @@
+package tpjava.tpjavacontrole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpJavaControleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
